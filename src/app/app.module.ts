6b7147175952f848a84621eed333e3c679fd2232
vivek <vivek@capitalQuotient.com>
2019-04-05 22:28:14 +0530

    full code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// router module
+import {RouterModule,Routes} from '@angular/router'
+// forms module
+import {FormsModule} from '@angular/forms'
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastModule } from 'ng2-toastr/ng2-toastr';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BlogViewComponent } from './blog-view/blog-view.component';
+import { BlogCreateComponent } from './blog-create/blog-create.component';
+import { BlogEditComponent } from './blog-edit/blog-edit.component';
+import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+// import statement from service
+import { HttpClientModule } from '@angular/common/http';
+import { BlogService } from './blog.service';
+import { BlogHttpService } from './blog-http.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogViewComponent,
+    BlogCreateComponent,
+    BlogEditComponent,
+    AboutComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastModule.forRoot(),
+    RouterModule.forRoot([
+      { path:'home', component:HomeComponent },
+      { path:'', redirectTo:'home', pathMatch:'full' },
+      { path: 'about', component: AboutComponent },
+      { path: 'blog/:blogId', component: BlogViewComponent },
+      { path: 'create', component: BlogCreateComponent },
+      { path: 'edit/:blogId', component: BlogEditComponent },
+      { path: '**', component: NotFoundComponent },
+    ])
   ],
-  providers: [],
+  providers: [BlogService,BlogHttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
